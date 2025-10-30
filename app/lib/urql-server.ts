23f8bffc4781a1ca2947e40737932ee1600b7e0e
gwyneth-pena <gwenpena21@gmail.com>
2025-10-30 17:20:07 +0800

    Clean up urql

--- app/lib/urql-server.ts
+++ app/lib/urql-server.ts
@@ -4,13 +4,14 @@ import { createClient, cacheExchange, fetchExchange, ssrExchange } from "urql";
 export async function createUrqlClient() {
   const ssrCache = ssrExchange({ isClient: false });
 
-  const cookieHeader = cookies().toString();
+  const cookieHeader = await cookies();
+  const cookie = cookieHeader?.toString();
 
   const client = createClient({
     url: process.env.NEXT_PUBLIC_GRAPH_API!,
     fetchOptions: {
       credentials: "include",
-      headers: cookieHeader ? { cookie: cookieHeader } : {},
+      headers: cookie ? { cookie } : {},
     },
     exchanges: [cacheExchange, ssrCache, fetchExchange],
   });
