48ea379d943e1af428f83b3dd7a6f22c99e5de11
gwyneth-pena <gwenpena21@gmail.com>
2025-11-13 13:38:19 +0800

    check cookie

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -10,7 +10,7 @@ export const metadata: Metadata = {
 };
 
 export default async function Login({ searchParams }: any) {
-  const sessionCookie = (await cookies()).get("session_id")?.value;
+  const sessionCookie = (await cookies()).get("session_id");
   console.log(sessionCookie);
   if (sessionCookie) {
     redirect("/");
