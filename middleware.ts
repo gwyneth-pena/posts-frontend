6968543e052a7d155299f01226f936e56c62b187
gwyneth-pena <gwenpena21@gmail.com>
2025-11-13 12:49:18 +0800

    Fix status

--- middleware.ts
+++ middleware.ts
@@ -2,7 +2,7 @@ import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
 export function middleware(request: NextRequest) {
-  const sessionId = request.cookies.get("session_id");
+  const sessionId = request.cookies.get("session_id")?.value;
   const { pathname } = request.nextUrl;
 
   if (
